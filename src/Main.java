28f334694f5b610250276c6b466838650ae9a5e7
romarsh <rom4rsh@gmail.com>
2024-10-11 20:51:15 +0300

    HW 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,102 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        // Задача 1
+        System.out.println("Задача 1");
+
+        int clientOs = 0;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+
+        // Задача 2
+        System.out.println("Задача 2");
+
+        clientOs = 1;
+        int clientDeviceYear = 2010;
+        if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
+        // Задача 3
+        System.out.println("Задача 3");
+
+        int year = 2024;
+        if (year > 1568 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " являеться високосным");
+        } else {
+            System.out.println(year + " не являеться високосным");
+        }
+
+
+         //Задача 4
+        System.out.println("Задача 4");
+
+        int deliveryDistance = 1;
+        int days = 0;
+        if (deliveryDistance <= 20) {
+            days = 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            days = 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            days = 3;
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней " + days);
+        }
+
+
+        // Задача 5
+        System.out.println("Задача 5");
+
+        int mothNumber = 16;
+        switch (mothNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Принадлежит к сезону осень");
+                break;
+            default:
+                    System.out.println("Такого месяца не существует");
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
